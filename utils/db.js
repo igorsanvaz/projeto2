03970056c76f03e9b790236268c24ffda49fc151
igorsanvaz <igorsanvaz@unifei.edu.br>
2024-06-30 19:26:52 -0300

    commit 11

--- utils/db.js
+++ utils/db.js
@@ -1,29 +1,13 @@
-const express = require("express");
-const { Client } = require('pg');
-const cors = require("cors");
-const bodyparser = require("body-parser");
-const config = require("./config");
+const { Pool } = require('pg');
 
-const app = express();
-app.use(express.json());
-app.use(cors());
-app.use(bodyparser.json());
-var conString = config.urlConnection;
-const client = new Client(conString)
+var conString = process.env.DATABASE_URL;
+const pool = new Pool(conString);
 
-client.connect((err) => {
-    if (err) {
-        return console.error('Não foi possível conectar ao banco.', err);
-    }
-    client.query('SELECT NOW()', (err, result) => {
-        if (err) {
-            return console.error('Erro ao executar a query.', err);
-        }
-        console.log(result.rows[0]);
-    });
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
 });
 
-app.get("/", (req, res) => {
-    console.log("Response ok.");
-    res.send("Ok - Servidor disponível.");
-});
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
